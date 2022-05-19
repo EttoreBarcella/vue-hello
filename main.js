0825bf3a3e03ea97f7c86742b8ed0beaec4f24e4
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-05-19 15:16:37 +0200

    added title and img

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let app = new Vue(
+    {
+        el: '#app',
+        data: {
+            message: "hello Vue : (",
+            image: {
+                img: "https://media-assets.wired.it/photos/615db8bcd6662d942971247d/master/w_1600,c_limit/1023.jpg",
+                nome: "pepe",
+            }
+        }
+    }
+)
